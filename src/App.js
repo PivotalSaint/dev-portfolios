a93df1a87be9a908dfb129b5e3bc9a3684f8e606
PivotalSaint <licensedmt@att.net>
2022-12-02 10:08:01 -0600

    starting to progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Nav from "./components/Nav";
+import About from "./components/AboutMe";
+import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+
+        <div>
+          <Portfolio></Portfolio>
+        </div>
+        <section>
+          <Resume></Resume>
+          <Contact></Contact>
+        </section>
+      </main>
     </div>
   );
 }
